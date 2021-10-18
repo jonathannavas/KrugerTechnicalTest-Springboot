23e531829d659302449d46214f8bea06ca268095
Jonathan Navas <jonathan.navas9@hotmail.com>
2021-10-17 21:52:39 -0500

    Import sql updated

--- src/main/java/com/jonathannavas/springboot/kruger/app/auth/ResourceServerConfig.java
+++ src/main/java/com/jonathannavas/springboot/kruger/app/auth/ResourceServerConfig.java
@@ -22,7 +22,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/usuarios").permitAll()
-		.anyRequest().authenticated()
+		.anyRequest().permitAll()
 		.and().cors().configurationSource(corsConfigurationSource());
 	}
 	
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,8 +7,8 @@ INSERT INTO vacunas(nombre) VALUES ('Jhonson&Jhonson');
 INSERT INTO roles(nombre) VALUES ('ROLE_USER');
 INSERT INTO roles(nombre) VALUES ('ROLE_ADMIN');
 
-INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id ) VALUES ('Administrador', 'Administrador', 'admin@admin', 1725635310, '1725635310', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
-INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id) VALUES ('Gabriel', 'Navas', 'gabriel@jonathan-navas.com', 1725635427, '1725635427', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
+INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id ) VALUES ('Administrador', 'Administrador', 'admin@admin.com', 1725630000, '1725630000', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
+INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id) VALUES ('Gabriel', 'Navas', 'gabriel@jonathan-navas.com', 1725630001, '1725630000', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
 
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 2);
