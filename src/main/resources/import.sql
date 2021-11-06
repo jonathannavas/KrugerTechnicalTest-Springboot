39aa0689947d28d20df31590b3cd1c1e0c89d97b
sdcollaguazo1 <48284831+sdcollaguazo1@users.noreply.github.com>
2021-11-06 11:02:39 -0500

    Update import.sql
    - Correccion usuario duplicado en import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,7 +8,7 @@ INSERT INTO roles(nombre) VALUES ('ROLE_USER');
 INSERT INTO roles(nombre) VALUES ('ROLE_ADMIN');
 
 INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id ) VALUES ('Administrador', 'Administrador', 'admin@admin.com', 1725630000, '1725630000', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
-INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id) VALUES ('Gabriel', 'Navas', 'gabriel@jonathan-navas.com', 1725630001, '1725630000', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
+INSERT INTO usuarios (nombre, apellido, email, cedula, username, password, estado, vacuna_id) VALUES ('Gabriel', 'Navas', 'gabriel@jonathan-navas.com', 1725630001, '1725630001', '$2a$10$Ek1MvuC56CzVNpsYOL935.kOIdFLcleoxcepaL9J79nmWK5Q7GVpK', false, 1);
 
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 2);
